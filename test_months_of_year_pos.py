b25b4944b944e868c89cb11a114c2da842b10bbb
mccahim2 <74020579+mccahim2@users.noreply.github.com>
2020-11-09 17:15:04 +0000

    Update test_months_of_year_pos.py

--- test_months_of_year_pos.py
+++ test_months_of_year_pos.py
@@ -6,6 +6,17 @@ from is_month_of_year import is_Month_Of_Year
 class TestMonths(unittest.TestCase):
 	def tester(self):
 		self.assertTrue(is_Month_Of_Year("January"))
+		self.assertTrue(is_Month_Of_Year("February"))
+		self.assertTrue(is_Month_Of_Year("March"))
+		self.assertTrue(is_Month_Of_Year("April"))
+		self.assertTrue(is_Month_Of_Year("May"))
+		self.assertTrue(is_Month_Of_Year("June"))
+		self.assertTrue(is_Month_Of_Year("July"))
+		self.assertTrue(is_Month_Of_Year("August"))
+		self.assertTrue(is_Month_Of_Year("September"))
+		self.assertTrue(is_Month_Of_Year("October"))
+		self.assertTrue(is_Month_Of_Year("November"))
+		self.assertTrue(is_Month_Of_Year("December"))
 
 if __name__== '__xmain__':
 	unittest.main()
