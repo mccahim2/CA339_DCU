e06dcfbc2598ed155af9aca14eef4cd89bdd42e4
mccahim2 <74020579+mccahim2@users.noreply.github.com>
2020-11-09 17:11:15 +0000

    Add files via upload

--- test_months_of_year_pos.py
+++ test_months_of_year_pos.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+import unittest
+from is_month_of_year import is_Month_Of_Year
+
+class TestMonths(unittest.TestCase):
+	def tester(self):
+		self.assertTrue(is_Month_Of_Year("January"))
+
+if __name__== '__xmain__':
+	unittest.main()
